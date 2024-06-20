b540fb32f71a7fd7457f1b61f97f1231c7c319c9
RafaelBallesta <Alumno@RAFAEL-BALLESTA>
2024-06-20 08:22:17 -0300

    Se implemento que los elementos que no tengan stock aparezcan tachado

--- src/componentes/FilaProducto.jsx
+++ src/componentes/FilaProducto.jsx
@@ -1,8 +1,12 @@
 
 export default function FilaProducto({ producto }) {
+    const estiloTachado = {
+        textDecoration: producto.stock === 0 ? 'line-through' : 'none'
+    };
+
     return (
         <tr>
-            <td>{producto.nombre}</td>
+            <td style={estiloTachado}>{producto.nombre}</td>
             <td>{producto.precio}</td>
             <td>{producto.stock}</td>
         </tr>
